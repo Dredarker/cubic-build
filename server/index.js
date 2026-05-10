fb1ad6451b3c048a7b080753162d27e4347560eb
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-05-10 20:23:25 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -321,7 +321,7 @@ wss.on("connection", (ws, req) => {
 							nickname,
  						}));
 						clients.get(id).nickname = nickname;
-						objects.set(id, new Player(nickname, 1.4, -11, new Obj(0, 0, 24, 80, "dynamic", "player", "#000000")));
+						objects.set(id, new Player(nickname, 1.4, -11, new Obj(0, 0, 24, 80, "dynamic", "player", data.color)));
 						msg("", clients, `${nickname} connected to game`);
 						clients.get(id).joined = true;
 						break;
